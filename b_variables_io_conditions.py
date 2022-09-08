ec7c36cf3faf52856da0578f0a9484e55daa8017
Michelle Jiang <m.jiang4@share.epsb.ca>
2022-09-08 09:51:52 -0600

    Created file b_variables_io_conditions.py added basic information and print command.

---
+++ b_variables_io_conditions.py
@@ -0,0 +1,20 @@
+#b_variables_io_conditions.py
+
+"""
+title: CS10 Concept Review and Language Translation
+author: Michelle Jiang
+date-created: 2022-09-08
+"""
+
+### VARIABLES ###
+
+STRING = '7' #double quotations are also permissible
+INTEGER = 7
+FLOAT = 7.0
+
+print(STRING)
+print(INTEGER)
+print(FLOAT)
+print(type(STRING))
+print(type(INTEGER))
+print(type(FLOAT))
