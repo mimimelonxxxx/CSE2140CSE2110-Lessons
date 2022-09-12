bcc4a4d0df667e3fd9f7c57acf212015e764dcd4
Michelle Jiang <m.jiang4@share.epsb.ca>
2022-09-12 10:14:45 -0600

    created d_8ball.py and started the program

---
+++ d_8ball.py
@@ -0,0 +1,21 @@
+#d_8ball.py
+"""
+title: Magic 8 Ball! 
+author: Michelle Jiang
+date-created: 2022-09-12
+"""
+"""
+Notes: 
+create 20 different responses 
+"""
+import random 
+
+### Intro ### 
+print("Shake the magic 8 ball! Seek to find ~*the answer in mind*~")
+
+# Input # 
+VALUE = input("Think your question, think it twice. Then ENTER in the 8 ball. (Press ENTER to shake the ball)")
+
+# Processing # 
+
+# Output # 
