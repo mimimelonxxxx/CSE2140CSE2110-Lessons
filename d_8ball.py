839e2caf1dbf135cf3587702b5406ab38e96666b
Michelle Jiang <m.jiang4@share.epsb.ca>
2022-09-13 10:01:38 -0600

    finished 8 ball program

--- d_8ball.py
+++ d_8ball.py
@@ -14,8 +14,50 @@ import random
 print("Shake the magic 8 ball! Seek to find ~*the answer in mind*~")
 
 # Input # 
-VALUE = input("Think your question, think it twice. Then ENTER in the 8 ball. (Press ENTER to shake the ball)")
+VALUE = input("Think your question, think it twice. Then ENTER in the 8 ball. (Just press ENTER to shake the ball)")
 
 # Processing # 
+if VALUE == "":
+    ANSWER = random.randint(1, 20)
 
-# Output # 
+# Output #
+    if ANSWER == 1:
+        print("⭐Certainly.⭐")
+    elif ANSWER == 2:
+        print("✨It is decidedly so.✨")
+    elif ANSWER == 3:
+        print("⭐Without a doubt.⭐")
+    elif ANSWER == 4:
+        print("✨Absolutely✨")
+    elif ANSWER == 5:
+        print("⭐Definitely⭐")
+    elif ANSWER == 6:
+        print("✨You can count on it✨")
+    elif ANSWER == 7:
+        print("⭐Yes⭐")
+    elif ANSWER == 8:
+        print("✨Most likely✨")
+    elif ANSWER == 9:
+        print("✨Outlook good.✨")
+    elif ANSWER == 10:
+        print("⭐Signs point to yes.⭐")
+    elif ANSWER == 11:
+        print("✨Cannot be determined.✨")
+    elif ANSWER == 12:
+        print("⭐Your mom⭐")
+    elif ANSWER == 13:
+        print("⭐Try again⭐")
+    elif ANSWER == 14:
+        print("✨Ask again later✨")
+    elif ANSWER == 15:
+        print("⭐Now is not a good time⭐")
+    elif ANSWER == 16:
+        print("✨ Sorry, but no. ✨")
+    elif ANSWER == 17:
+        print("~*Clear your mind*~ and ask again")
+    elif ANSWER == 18:
+        print("⭐The stars do not align⭐")
+    elif ANSWER == 19:
+        print("✨My sources say no✨")
+    else:
+        print("~*It is not fated*~")
